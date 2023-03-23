9a192a0d0e1d465a3baea9b5604ec1225a8d4d04
Wiedjaja Atmadja <swiedjaja@binus.ac.id>
2023-03-23 14:49:12 +0700

    Step 3 build text from list

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,19 @@ class MainApp extends StatelessWidget {
     'What\'s your favorite book?',
     'What\'s your favorite game?',
     'What\'s your favorite sport?',
+    'What\'s your favorite hobby?',
+    'What\'s your favorite drink?',
+    'What\'s your favorite dessert?',
+    'What\'s your favorite fruit?',
+    'What\'s your favorite vegetable?',
+    'What\'s your favorite season?',
+    'What\'s your favorite holiday?',
+    'What\'s your favorite place?',
+    'What\'s your favorite country?',
+    'What\'s your favorite city?',
+    'What\'s your favorite state?',
+    'What\'s your favorite province?',
+    'What\'s your favorite island?',
   ];
   MainApp({super.key});
 
@@ -25,9 +38,7 @@ class MainApp extends StatelessWidget {
           title: const Text('My First App'),
         ),
         body: Column(
-          children: [
-            Text(questions[0]),
-          ],
+          children: [...(questions.map((question) => Text(question)).toList())],
         ),
       ),
     );
